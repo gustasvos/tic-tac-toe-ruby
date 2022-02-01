93a7b2ab30981af6ea7b7bcf2787c0ef4c2fd0f3
Gustavo Ribeiro <gustavorib.rosa@gmail.com>
2022-02-01 13:08:07 -0300

    requires

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,6 +3,7 @@ require_relative "tic_tac_toe/version"
 require_relative "./tic_tac_toe/player.rb"
 require_relative "./tic_tac_toe/game.rb"
 require_relative "./tic_tac_toe/board.rb"
+require_relative "./tic_tac_toe/core_extensions.rb"
  
 module TicTacToe
 end
